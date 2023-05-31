1f25b3ddff714a9c60b0e3a41375cf7d55787396
dzzprd <70172465+dzzprd@users.noreply.github.com>
2023-05-31 18:26:41 +0200

    Quick cleanup

--- Assets/Scripts/Truck/TruckManager.cs
+++ Assets/Scripts/Truck/TruckManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TruckMovement : MonoBehaviour
+public class TruckManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
