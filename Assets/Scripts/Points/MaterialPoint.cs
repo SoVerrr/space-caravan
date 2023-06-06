753082816173afdd6b40d7cd77dbd232e591b7df
xMikuuu <m.polatynski9@gmail.com>
2023-06-06 18:45:30 +0200

    Processing Points info
    added special function to display info about processing points

--- Assets/Scripts/Points/MaterialPoint.cs
+++ Assets/Scripts/Points/MaterialPoint.cs
@@ -7,13 +7,11 @@ public class MaterialPoint : Point
     public static List<GameObject> materialPointList;
     [SerializeField] TextMeshProUGUI textElement;
     private PointDataMaterial materialData;
-    public int prodRate;
     override public void InstantiatePoint(int x, int y, PointData data)
     {
         materialData = new PointDataMaterial();
-        prodRate = materialData.GetProductionRate();
-        Debug.Log("coords: " + x + " " + y + " type: " + materialData.GetProductionRate());
-        textElement.text = materialData.GetProductionRate().ToString();
+        //Debug.Log("coords: " + x + " " + y + " type: " + materialData.GetProductionRate());
+        textElement.text = materialData.GetMaterialType().ToString() + "\n" + materialData.GetProductionRate().ToString();
         var point = Instantiate(gameObject, new Vector3(x, 0, y), Quaternion.identity);
         materialPointList.Add(point);
     }
--- Assets/Scripts/Points/ProcessingResult.cs
+++ Assets/Scripts/Points/ProcessingResult.cs
@@ -8,4 +8,17 @@ public class ProcessingResult : ScriptableObject
     [SerializeField] string resultName;
     [SerializeField] string[] materialsNeeded;
     [SerializeField] int[] numberOfMaterialsNeeded;
+
+    public string GetResultName()
+    {       
+        return resultName;
+    }
+    public string[] GetMaterialNeeded()
+    {
+        return materialsNeeded;
+    }
+    public int[] GetNumberOfMaterialNeeded()
+    {
+        return numberOfMaterialsNeeded;
+    }
 }
