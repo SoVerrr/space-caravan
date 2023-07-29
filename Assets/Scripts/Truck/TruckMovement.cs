ce73e271c31d40f6be019519637f3335b918d40e
SoVerrr <82769474+SoVerrr@users.noreply.github.com>
2023-07-29 18:08:29 +0200

    Truck movement DONE

--- Assets/Scripts/Truck/TruckMovement.cs
+++ Assets/Scripts/Truck/TruckMovement.cs
@@ -8,32 +8,53 @@ public class TruckMovement : MonoBehaviour
     [SerializeField] SpaceGrid grid;
     public void SendTruck(Point startingPoint, Point endPoint)
     {
-        Vector2Int startPos = startingPoint.GetPointPosition();
+        /*Vector2Int startPos = startingPoint.GetPointPosition();
         Vector2Int endPos = endPoint.GetPointPosition();
         Debug.Log($"Startpos:{startPos} | endpos: {endPos}");
         List<Vector2Int> path = Pathfinding.AStarSearch(grid, startPos, endPos, true);
-        path.Reverse();
-        StartCoroutine(MoveTruckToPosition(path));
+        path.Reverse();*/
     }
-    IEnumerator MoveTruckToPosition(List<Vector2Int> path)
+    IEnumerator MoveTruckToPosition(List<Point> route)
     {
-        foreach (Vector2Int pathItem in path)
+        for (int i = 0; i < route.Count; i++)
         {
-            Vector3 target = new Vector3(pathItem.x, 0, pathItem.y);
-            while (transform.position != target)
+            List<Vector2Int> path = new List<Vector2Int>();
+            if (i < route.Count - 1)
             {
-                transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime);
-                yield return null;
+                Vector2Int startPos = route[i].GetPointPosition();
+                Vector2Int endPos = route[i + 1].GetPointPosition();
+                Debug.Log($"Startpos:{startPos} | endpos: {endPos}");
+                path = Pathfinding.AStarSearch(grid, startPos, endPos, true);
+            }
+            else
+            {
+                Vector2Int startPos = route[i].GetPointPosition();
+                Vector2Int endPos = route[0].GetPointPosition();
+                Debug.Log($"Startpos:{startPos} | endpos: {endPos}");
+                path = Pathfinding.AStarSearch(grid, startPos, endPos, true);
+            }
+            path.Reverse();
+
+            foreach (Vector2Int pathItem in path)
+            {
+                Vector3 target = new Vector3(pathItem.x, 0, pathItem.y);
+                while (transform.position != target)
+                {
+                    transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime);
+                    yield return null;
+                }
             }
         }
+        
+
     }
     public void SendOnRoute(TradeRoute tradeRoute)
     {
         List<Point> route = tradeRoute.GetRoute();
-        for (int i = 0; i < route.Count - 1; i++)
+        StartCoroutine(MoveTruckToPosition(route));
+        /*for (int i = 0; i < route.Count - 1; i++)
         {
-
             SendTruck(route[i], route[i + 1]);
-        }
+        }*/
     }
 }
