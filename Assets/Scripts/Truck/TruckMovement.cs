580cb6a77ec8d5a74a825526bfd84e3d4a24d85f
SoVerrr <82769474+SoVerrr@users.noreply.github.com>
2023-07-30 18:59:46 +0200

    Code fixes
    For better readability

--- Assets/Scripts/Truck/TruckMovement.cs
+++ Assets/Scripts/Truck/TruckMovement.cs
@@ -11,10 +11,12 @@ public class TruckMovement : MonoBehaviour
     private List<Vector2Int> currentPath;
     private int currentPathCheckpoint = 0;
     private int currentRouteCheckpoint = 1;
+
     private void Awake()
     {
         currentRoute = new List<Point>();
     }
+
     public void SendOnRoute(TradeRoute tradeRoute)
     {
         currentRoute = tradeRoute.GetRoute();
@@ -22,58 +24,80 @@ public class TruckMovement : MonoBehaviour
         currentPathCheckpoint++;
         isOnRoute = true;
     }
+
     private List<Vector2Int> GetPathToPoint(int pointIndex)
     {
         List<Vector2Int> path = Pathfinding.AStarSearch(grid, currentRoute[pointIndex - 1].GetPointPosition(), currentRoute[pointIndex].GetPointPosition(), true);
         path.Reverse();
         return path;
     }
+
     private List<Vector2Int> GetPathToBeginning()
     {
         return Pathfinding.AStarSearch(grid, currentRoute[0].GetPointPosition(), currentRoute[currentRoute.Count - 1].GetPointPosition(), true);
     }
+
     private bool HasReachedCheckpoint(int index)
     {
         return transform.position == new Vector3(currentPath[index].x, 0, currentPath[index].y);
     }
+
     private bool HasFinishedRoute()
     {
         return currentRouteCheckpoint == currentRoute.Count + 1;
     }
+
     private void GoToCheckpoint(int index)
     {
         transform.position = Vector3.MoveTowards(transform.position, new Vector3(currentPath[index].x, 0, currentPath[index].y), Time.deltaTime);
     }
-    private void FixedUpdate()
+
+    private bool NextCheckpoint()
     {
-
-        if (isOnRoute)
+        if(currentPathCheckpoint < currentPath.Count - 1)
         {
-            if(!HasReachedCheckpoint(currentPathCheckpoint))
-                GoToCheckpoint(currentPathCheckpoint);
-            if (HasReachedCheckpoint(currentPathCheckpoint) && currentPathCheckpoint < currentPath.Count - 1)
-            {
-                currentPathCheckpoint++;
-
-            }
-            else if (HasReachedCheckpoint(currentPathCheckpoint))
-            {
-                currentPathCheckpoint = 1;
-                currentRouteCheckpoint++;
-                if (HasFinishedRoute())
-                {
-                    isOnRoute = false;
-                    return;
-                }
-                if (currentRouteCheckpoint == currentRoute.Count)
-                    currentPath = GetPathToBeginning();
-                else
-                    currentPath = GetPathToPoint(currentRouteCheckpoint);
-            }
-            
+            currentPathCheckpoint++;
+            return true;
         }
+        currentPathCheckpoint = 1;
+        return false;
+    }
 
-
+    private void UpdatePath()
+    {
+        currentRouteCheckpoint++;
+        if (HasFinishedRoute())
+        {
+            isOnRoute = false;
+            return;
+        }
+        if (currentRouteCheckpoint == currentRoute.Count)
+        {
+            currentPath = GetPathToBeginning();
+        }
+        else
+        {
+            currentPath = GetPathToPoint(currentRouteCheckpoint);
+        }
+    }
+    private void Update()
+    {
+        if (!isOnRoute)
+        {
+            return;
+        }
+        if (!HasReachedCheckpoint(currentPathCheckpoint))
+        {
+            GoToCheckpoint(currentPathCheckpoint);
+        }
+        else if (HasReachedCheckpoint(currentPathCheckpoint))
+        {
+            if (!NextCheckpoint())
+            {
+                UpdatePath();
+            }
+        }
+            
 
     }
 }
