330405b9f8c38fb2108b20d9c07a0e562681c8ed
SoVerrr <82769474+SoVerrr@users.noreply.github.com>
2023-07-30 17:30:26 +0200

    Truck movement changed from coroutine to update

--- Assets/Scripts/Truck/TruckMovement.cs
+++ Assets/Scripts/Truck/TruckMovement.cs
@@ -6,44 +6,74 @@ public class TruckMovement : MonoBehaviour
 {
 
     [SerializeField] SpaceGrid grid;
-
-    IEnumerator MoveTruckToPosition(List<Point> route)
+    private bool isOnRoute = false;
+    private List<Point> currentRoute;
+    private List<Vector2Int> currentPath;
+    private int currentPathCheckpoint = 0;
+    private int currentRouteCheckpoint = 1;
+    private void Awake()
     {
-        for (int i = 0; i < route.Count; i++)
-        {
-            List<Vector2Int> path = new List<Vector2Int>();
-            if (i < route.Count - 1)
-            {
-                Vector2Int startPos = route[i].GetPointPosition();
-                Vector2Int endPos = route[i + 1].GetPointPosition();
-                Debug.Log($"Startpos:{startPos} | endpos: {endPos}");
-                path = Pathfinding.AStarSearch(grid, startPos, endPos, true);
-            }
-            else
-            {
-                Vector2Int startPos = route[i].GetPointPosition();
-                Vector2Int endPos = route[0].GetPointPosition();
-                Debug.Log($"Startpos:{startPos} | endpos: {endPos}");
-                path = Pathfinding.AStarSearch(grid, startPos, endPos, true);
-            }
-            path.Reverse();
-
-            foreach (Vector2Int pathItem in path)
-            {
-                Vector3 target = new Vector3(pathItem.x, 0, pathItem.y);
-                while (transform.position != target)
-                {
-                    transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime);
-                    yield return null;
-                }
-            }
-        }
-        
-
+        currentRoute = new List<Point>();
     }
     public void SendOnRoute(TradeRoute tradeRoute)
     {
-        List<Point> route = tradeRoute.GetRoute();
-        StartCoroutine(MoveTruckToPosition(route));
+        currentRoute = tradeRoute.GetRoute();
+        currentPath = GetPathToPoint(1);
+        currentPathCheckpoint++;
+        isOnRoute = true;
+    }
+    private List<Vector2Int> GetPathToPoint(int pointIndex)
+    {
+        List<Vector2Int> path = Pathfinding.AStarSearch(grid, currentRoute[pointIndex - 1].GetPointPosition(), currentRoute[pointIndex].GetPointPosition(), true);
+        path.Reverse();
+        return path;
+    }
+    private List<Vector2Int> GetPathToBeginning()
+    {
+        return Pathfinding.AStarSearch(grid, currentRoute[0].GetPointPosition(), currentRoute[currentRoute.Count - 1].GetPointPosition(), true);
+    }
+    private bool HasReachedCheckpoint(int index)
+    {
+        return transform.position == new Vector3(currentPath[index].x, 0, currentPath[index].y);
+    }
+    private bool HasFinishedRoute()
+    {
+        return currentRouteCheckpoint == currentRoute.Count + 1;
+    }
+    private void GoToCheckpoint(int index)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(currentPath[index].x, 0, currentPath[index].y), Time.deltaTime);
+    }
+    private void FixedUpdate()
+    {
+
+        if (isOnRoute)
+        {
+            if(!HasReachedCheckpoint(currentPathCheckpoint))
+                GoToCheckpoint(currentPathCheckpoint);
+            if (HasReachedCheckpoint(currentPathCheckpoint) && currentPathCheckpoint < currentPath.Count - 1)
+            {
+                currentPathCheckpoint++;
+
+            }
+            else if (HasReachedCheckpoint(currentPathCheckpoint))
+            {
+                currentPathCheckpoint = 1;
+                currentRouteCheckpoint++;
+                if (HasFinishedRoute())
+                {
+                    isOnRoute = false;
+                    return;
+                }
+                if (currentRouteCheckpoint == currentRoute.Count)
+                    currentPath = GetPathToBeginning();
+                else
+                    currentPath = GetPathToPoint(currentRouteCheckpoint);
+            }
+            
+        }
+
+
+
     }
 }
